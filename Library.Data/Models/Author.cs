ba727c978099de1f153a14452b61776316581a86
Maksim <maxpotapnev2003@gmail.com>
2024-09-30 14:50:40 +0300

    Add Database

---
+++ Library.Data/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Motherland { get; set; }
+    }
+}
---
+++ Library.Data/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public DateTime ExpectedReturnDate  { get; set; }
+    }
+}
