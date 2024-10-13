89686347684f0e356a3aa7d5a4148c7629134f74
Maksim <maxpotapnev2003@gmail.com>
2024-10-13 14:46:49 +0300

    Add searching by name

---
+++ Library/Models/Books/SearchByNameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Library.Data.Models;
+using Library.Models.ValidationAttributes;
+
+namespace Library.Models.Books
+{
+    public class SearchByNameViewModel
+    {
+        public string Name { get; set; }
+        public Book? SearchedBook { get; set; }
+        public bool? HasCover { get; set; }
+    }
+}
