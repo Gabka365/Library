300f4a5b9c1aa76e0f0bf4814d8e99a5c6b07373
Maksim <maxpotapnev2003@gmail.com>
2024-10-13 17:03:36 +0300

    Add modal window for confirm deleting

---
+++ Library/wwwroot/js/Books/books.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+
+    $('#closeModal').click(function () {
+        $('#myModal').modal('hide');
+    });
+
+    $('#confirmDelete').click(function () {
+        $('#myModal').modal('hide'); 
+    });
+});
